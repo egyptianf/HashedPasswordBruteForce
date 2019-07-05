5ca4a9d5f95e14173f29dd7f9c1ec861848003e3
egyptianf <walied2@protonmail.com>
2019-07-05 18:41:56 +0200

    genalized!

--- crack.c
+++ crack.c
@@ -3,6 +3,7 @@
 #include<crypt.h>
 #include<string.h>
 #include<math.h>
+#include <stdlib.h>
 int error(){
     printf("Usage: ./crack hash\n");
     return 1;
@@ -21,66 +22,68 @@ int main(int argc, char** argv)
     if(argc != 2)
         return error();
 
-    // char *hashed = argv[1], *salt = "00";
-    // char plaintext[5]= "AAAAA";//A:65 in ASCII and Z:90, a:97 and z:122
-    // char *hashed_plaintext = crypt(plaintext, salt);
+    char *hashed = argv[1], *salt = "00";
+    char plaintext[5]= "AAAAA";//A:65 in ASCII and Z:90, a:97 and z:122
+    char *hashed_plaintext = crypt(plaintext, salt);
 
-    // if(strcmp(hashed_plaintext, hashed) == 0)
-    // {
-    //     puts(plaintext);
-    //     return 0;
-    // }
+    if(strcmp(hashed_plaintext, hashed) == 0)
+    {
+        puts(plaintext);
+        return 0;
+    }
 
     //If we're using a password of 5 digits
-    int digits =5;
-    int digit;
-    //Start from right to left by changing the digit by to all its possible values
-    // for(digit=4; digit>=0; digit--)
-    // {
-    //     //2 for loops; one for uppercase and the other for lowercase letters
-    //     int upper;
-    //     for(upper=65; upper<91; upper++)
-    //     {
-    //         plaintext[digit] = upper;
-    //         //puts(plaintext);
-    //         hashed_plaintext = crypt(plaintext, salt);
-    //         if(match(plaintext, hashed_plaintext, hashed))
-    //             return 0;
-    //     }
-    //     int lower;
-    //     for(lower=97; lower<123; lower++)
-    //     {
-    //         plaintext[digit] = lower;
-    //         //puts(plaintext);
-    //         hashed_plaintext = crypt(plaintext, salt);
-    //         if(match(plaintext, hashed_plaintext, hashed))
-    //             return 0;
-    //     }
-    // }
-
-    //Testing
-    char txt[3] = "000";
-    digits = strlen(txt)+0.0;
+    int digits;
+    digits = strlen(plaintext)+0.0;
+    
+    int digit1=0, digit2=0, digit3=0, digit4=0, digit5=0;
+    int *ascii_chars = malloc(sizeof(int) * digits);
+    //Inititialization
     int i;
-    int digit1=0, digit2=0, digit3=0;
-    int permutations = (int)pow(10.0, digits);
+    for(i=0; i<digits; i++)
+        ascii_chars[i] = 0;
+
+
+    int permutations =(int) pow(10.0, digits);
     for(i=1; i<= permutations; i++)
     {
         //If conditions for each digit except the least significant digit
-        //Leaset significant bit digit
-        txt[2] = digit1 + '0';
-        digit1 = (digit1+1)%10;
 
-        txt[1] = digit2 + '0';
-        if((i%10) == 0)
-            digit2 = (digit2+1)%10;
+        int index;
+        for(index= digits-1; index>=0; index--)
+        {
+            plaintext[index] = ascii_chars[index] + '0';
+            if( (i % ((int) pow(10.0,(digits-1-index)))) == 0)
+                ascii_chars[index] = (ascii_chars[index] + 1)%10;
+        }
+
+
+
+
+
+
+        // //Leaset significant bit digit
+        // plaintext[4] = digit1 + '0';
+        // digit1 = (digit1+1)%10;
+
+        // plaintext[3] = digit2 + '0';
+        // if((i%10) == 0)
+        //     digit2 = (digit2+1)%10;
         
-        txt[0] = digit3 + '0';
-        if((i%100)==0)
-            digit3 = (digit3+1)%10;
+        // plaintext[2] = digit3 + '0';
+        // if((i%100)==0)
+        //     digit3 = (digit3+1)%10;
     
-        puts(txt);
-    }
+        // plaintext[1] = digit4 + '0';
+        // if((i%1000)==0)
+        //     digit4 = (digit4+1)%10;
+        // plaintext[0] = digit5 + '0';
+        // if((i%10000)==0)
+        //     digit5 = (digit5+1)%10;
 
+
+        //puts(plaintext);
+    }
+    free(ascii_chars);
     return 0;
 }
