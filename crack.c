353ec876bd954a448465f0e946e3b23d3beeb63d
egyptianf <walied2@protonmail.com>
2019-07-05 21:25:25 +0200

    some minor modifications to optimize the code

--- crack.c
+++ crack.c
@@ -11,10 +11,7 @@ int error(){
 _Bool match(char *plaintext, char *hashed_plaintext, char *hashed)
 {
     if(strcmp(hashed_plaintext, hashed)==0)
-    {
-        puts(plaintext);
         return 1;
-    }
     return 0;
 }
 int get_next_char(int current_char)
@@ -27,7 +24,7 @@ int get_next_char(int current_char)
     return next_char;
 }
 //Returns 1 when found
-_Bool brute_force(int digits, int base, char* plaintext, char *hashed, char *hashed_plaintext)
+char *brute_force(int digits, int base, char* plaintext, char *salt, char *hashed, char *hashed_plaintext)
 {  
     int *ascii_chars = malloc(sizeof(int) * digits);
     //Inititialization
@@ -45,12 +42,13 @@ _Bool brute_force(int digits, int base, char* plaintext, char *hashed, char *has
             if( (i % ((int) pow(base,(digits-1-index)))) == 0)
                 ascii_chars[index] = get_next_char(ascii_chars[index]);//need to be changed
         }
+        hashed_plaintext = crypt(plaintext, salt);
         if(match(plaintext, hashed_plaintext, hashed))
-            return 1;
+            return plaintext;
         //puts(plaintext);
     }
     free(ascii_chars);
-    return 0;
+    return NULL;
 }
 int main(int argc, char** argv)
 {
@@ -69,7 +67,9 @@ int main(int argc, char** argv)
     //If we're using a password of 5 digits
     int digits, base=52;//(26+26)for capital and small english letters.
     digits = strlen(plaintext)+0.0;
-    if(brute_force(digits, plaintext, base))
-        return 0;
-    return 1;
+    char *password = brute_force(digits, base, plaintext, salt, hashed, hashed_plaintext);
+    if(password == NULL)
+        return 1;
+    puts(password);
+    return 0;
 }
